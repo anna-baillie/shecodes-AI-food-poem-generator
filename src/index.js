425e04295628aebd9c37288d8d20fd187340679e
aebailu <anna.baillie@outlook.com>
2024-12-23 11:02:00 +0000

    HTML response format
    Cleaned up the format of the AI response.

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generatePoem(event) {
   let foodFocus = document.querySelector("#input-text");
   let apiKey = "8o2ff7498032b5ae4f9bt64daad694e7";
   let context =
-    "You are a poet AI Assistant that tells short poems. The poem must not exceed 6 lines. The poem must be provided in HTML format. Example: <p>poem line </p>. Sign the poem at the end with SheCodes AI, inside <strong> tag.";
+    "You are a poet AI Assistant that tells short poems. Generate a 4-line poem in basic HTML, separate each line with a <br />. Do not include HTML tags at the beginning or end. Sign the poem at the end with SheCodes AI, inside <strong> tag.";
   let prompt = `Generate a unique poem about ${foodFocus.value}.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   axios.get(apiUrl).then(showPoem);
