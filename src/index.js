ed32c20b87d51e0ed9d8fba78913995b065b0ebc
aebailu <anna.baillie@outlook.com>
2024-12-23 10:42:25 +0000

    Added javascript functionality
    'Generating poem' message loads with typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// alert("One-stop-shop for poems about food");
+function generatePoem(event) {
+  event.preventDefault();
+
+  //   let poemContainer = document.querySelector(".poem-holder");
+  //   poemContainer.innerHTML = "Generating poem...";
+
+  new Typewriter(".poem-holder", {
+    strings: "Generating poem...",
+    autoStart: true,
+    delay: 60,
+    cursor: null,
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator");
+poemFormElement.addEventListener("submit", generatePoem);
